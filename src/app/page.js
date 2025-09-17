4bbfd01526b625145df21b5763904f9516169005
MMunim <mmunim9.9.01@gmail.com>
2025-09-17 12:08:07 +0600

    error fixes

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
 
           <button>Don't Click</button>
           <button className=" border-2 py-3 px-5">Click Me</button>
-          <button onClick={alert("besi jore chap diye felechen")} className=" border-2 py-3 px-5">Jore click kore den</button>
+          {/* <button onClick={() =>alert("besi jore chap diye felechen")} className=" border-2 py-3 px-5">Jore click kore den</button> */}
 
       </main>
     </div>
