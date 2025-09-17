77794cbff7f9cadbcfe42c1013b28de4eb2bdefb
Raheel Arfeen <raheelarfeen@gmail.com>
2025-09-17 17:59:43 +0600

    fixed the conditional header & footer thing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 // app/layout.js
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConditionalHeader from "./ConditionalHeader";
+import ConditionalFooter from "./ConditionalFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col min-h-screen justify-between">
+          <ConditionalHeader /> {/* Header will handle path logic */}
+          {children}
+          <ConditionalFooter /> {/* Footer will handle path logic */}
+        </div>
       </body>
     </html>
   );
