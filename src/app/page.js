bc708ad40d4d5597b2fc07f3e54dd602e2f6a537
Alireja-khan <alirejakhan18@gmail.com>
2025-09-18 20:06:40 +0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-6xl font-bold">Welcome To MechaLink</h1>
-        <p>👉 You only run create-react-app (or npm create vite@latest, npx next, etc.) if you are starting a brand new project.
-          So for cloning and running an existing repo → just clone, install, and run.
-          Do you want me to also show you what to do if the cloned repo uses Vite or Next.js instead of CRA?</p>
+        <p>Team Thor has 6 members</p>
       </main>
     </div>
   );
