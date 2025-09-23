c0311a20a5ae5f339b4d4680352adbfc4edd587e
mdrahimultahsin <mdrahimultahsin@gmail.com>
2025-09-23 22:33:32 +0600

    added api to get single request by using _id

---
+++ src/app/api/service-request/[id]/route.js
@@ -0,0 +1,29 @@
+import dbConnect from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+
+export async function GET(req, { params }) {
+  try {
+    const { id } = params;
+
+  
+  const collection = await dbConnect("serviceRequests");
+
+    const request = await collection.findOne({ _id: new ObjectId(id) });
+
+    if (!request) {
+      return new Response(
+        JSON.stringify({ error: "Service request not found" }),
+        { status: 404 }
+      );
+    }
+
+    return new Response(JSON.stringify(request), { status: 200 });
+  } catch (error) {
+    console.error("❌ Error fetching service request:", error);
+    return new Response(
+      JSON.stringify({ error: "Failed to fetch service request" }),
+      { status: 500 }
+    );
+  }
+}
